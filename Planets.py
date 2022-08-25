7ebd2c8bf089e55d174cbd9df305b763e72d7e6c
joshuad123 <66506393+joshuad123@users.noreply.github.com>
2022-08-25 10:34:45 +0530

    Add files via upload

---
+++ Planets.py
@@ -0,0 +1,23 @@
+import cv2
+
+img = cv2.imread("solar-system.jpg")
+
+cv2.putText(img , "Mercury" , (120,190), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Venus" , (190,170), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Earth" , (280,150), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Mars" , (390,160), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Jupiter" , (560,50), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Saturn" , (750,90), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Uranus" , (960,120), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.putText(img , "Neptune" , (1130,130), fontFace= cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=0.5, color= (255 , 255 , 255))
+
+cv2.imshow("Rocket", img)
+
+cv2.waitKey(0)
